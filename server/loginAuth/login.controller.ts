e0841c5458d5d36be91160b823a20f64207dce2c
unknown <jazzygee36gmail.com>
2023-04-14 21:43:32 +0100

    second

--- server/loginAuth/login.controller.ts
+++ server/loginAuth/login.controller.ts
@@ -1,13 +1,76 @@
-import { Request, Response } from "express"
-import UserSchema from './login.schema'
+import { Request, Response } from "express";
+import UserSchema from "./login.schema";
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
 
-export const UserResgister = async(req: Request, res: Response) => {
-try{
-    const body = req.body
-    const user = await UserSchema.create(body)
-    return res.status(200).json(user)
+export const UserResgister = async (req: Request, res: Response) => {
+  try {
+    const body = req.body;
+    const hashPassword = await bcrypt.hash(req.body.password, 10);
+    const user = await UserSchema.create({ ...body, password: hashPassword });
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
 
-}catch(error){
-    return res.status(500).send(error)
-}
-}
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const user = await UserSchema.find();
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
+export const getUsersById = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const user = await UserSchema.findById(id);
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
+
+export const updateUserbyId = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const user = await UserSchema.findOneAndUpdate({ id });
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
+export const deleteUserbyId = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const user = await UserSchema.deleteOne({ id });
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
+
+export const userLogin = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(401).json({ message: "email and password required" });
+  }
+  try {
+    const user = await UserSchema.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "email not found" });
+    } else {
+      const confirmPwd = await bcrypt.compare(password, user.password);
+      if (!confirmPwd) {
+        return res.status(400).json({ message: "password not match" });
+      } else {
+        const token = jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET!)
+        return res.status(200).json({ token:token});
+      }
+    }
+    
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
--- server/loginAuth/login.routes.ts
+++ server/loginAuth/login.routes.ts
@@ -1,7 +1,19 @@
-import express from 'express'
-import { UserResgister } from './login.controller'
-const route = express.Router()
+import express from "express";
+import {
+  UserResgister,
+  getAllUsers,
+  getUsersById,
+  deleteUserbyId,
+  updateUserbyId,
+  userLogin
+} from "./login.controller";
+const route = express.Router();
 
-route.post('/register', UserResgister)
+route.post("/register", UserResgister);
+route.get("/users", getAllUsers);
+route.get("/user/:id", getUsersById);
+route.delete("/user/:id", deleteUserbyId);
+route.patch("/user/:id", updateUserbyId);
+route.post("/user/login", userLogin);
 
-export default route
+export default route;
